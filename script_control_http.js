416c3d27a46c5ad0b090860d6ed65931184dba4c
Eric Mill <eric@konklone.com>
2015-02-01 23:17:53 -0500

    one more test moved

--- script_control_http.js
+++ script_control_http.js
@@ -1,10 +1,10 @@
 
 /**
   accessed at:
-  https://cdn.konklone.io/script_control_https.js
+  http://plain-cdn.konklone.io/script_control_http.js
 
   used by to pass the test at:
   http://konklone.io/cdn-testing/
 **/
 
-done('script_control_https');
+done('script_control_http');
